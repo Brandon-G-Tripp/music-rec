e3cf94c23417b51696cfaa58f61891b5e70e86ab
Brandon Tripp <brandongtripp@gmail.com>
2024-01-26 18:09:34 -0700

    Implemented basic test for load csv data and the implementation logic

--- test/music_rec/core_test.clj
+++ test/music_rec/core_test.clj
@@ -2,5 +2,33 @@
   (:require [clojure.test :refer :all]
             [music-rec.core :refer :all]))
 
-(deftest test-load-data 
-    (is (= {:artists ["Artist 1"]} (load-data "test/resources/sample.csv"))))
+(deftest test-load-csv
+  ;; Read sample data as map for expectations
+  (let [result-tracks (load-csv "fma_metadata/tracks.csv")
+        headers (keys (first result-tracks))
+        sample (take 3 result-tracks)]
+
+    (is (= headers
+           (keys (first sample))))
+
+    (testing "Sample has expected number of rows"
+      (is (= 3 (count sample))))
+
+    (testing "Row 1 matches full dataset"
+      (is (= (first result-tracks)
+             (first sample)))))
+
+  (let [result-echonest (load-csv "fma_metadata/echonest.csv")
+        headers (keys (first result-echonest))
+        sample (take 3 result-echonest)]
+
+    (is (= headers
+           (keys (first sample))))
+
+    (testing "Sample has expected number of rows"
+      (is (= 3 (count sample))))
+
+    (testing "Row 1 matches full dataset"
+      (is (= (first result-echonest)
+             (first sample))))))
+
