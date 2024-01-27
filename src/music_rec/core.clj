e85997286e8e39dc7b101dbbb099d2c2dd3903e3
Brandon Tripp <brandongtripp@gmail.com>
2024-01-26 18:09:34 -0700

    Implemented basic test for load csv data and the implementation logic

--- src/music_rec/core.clj
+++ src/music_rec/core.clj
@@ -1,5 +1,20 @@
 (ns music-rec.core
-  (:require [clojure.data.csv :as csv]))
+  (:require 
+    [clojure.data.csv :as csv]
+    [clojure.java.io :as io]))
 
-(defn load-data [file-path]
-  {:artists ["Artist 1"]})
+(def tracks-csv "fma_metadata/tracks.csv")
+(def echonest-csv "fma_metadata/echonest.csv")
+
+
+(defn parse-file [file-path]
+  (let [rows (with-open [ reader (io/reader file-path)]
+      (doall
+        (csv/read-csv reader)))]
+    (map #(zipmap
+            (:headers (first rows))
+            %)
+         (rest rows))))
+
+(defn load-csv [file-path]
+    (parse-file file-path))
